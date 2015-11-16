9d7bb8bac3bb814ef8d155ca1399ff434775e506
emilien puget <puget.e@gmail.com>
2015-11-16 17:57:51 +0100

    steamCmd check

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ if os.path.isfile('settings.cfg'):
     parser.read('settings.cfg')
 
     steam_cmd_path = parser.get('parameters', 'steam_cmd_path')
-
-    print(os.path.dirname(__file__))
-    steam = steamCmd.SteamCmd(os.path.dirname(__file__) + '/' + steam_cmd_path)
-    steam.get_installed_game()
+    path = os.path.dirname(__file__) + '/' + steam_cmd_path
+    if os.path.isfile(path):
+        steam = steamCmd.SteamCmd(path)
+        steam.get_installed_game()
+    else:
+        print("Can't find steamCmd at '" + path + "'")
 else:
     exit()
